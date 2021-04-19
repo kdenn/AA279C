878eb5c7be79422b5f1dd5d371fc7cbdeff0ba30
Kaitlin Dennison <kdenn@stanford.edu>
2021-04-19 11:53:33 -0700

    update to handle array of values

--- propagation/dcm2eulax.m
+++ propagation/dcm2eulax.m
@@ -1,16 +1,22 @@
-function [e_vec,phi] = dcm2eulax(A)
+function e_vec = dcm2eulax(A)
     % convert the dcm to Eular axis/angle representation
 
-    phi = acos(0.5 * (trace(A) - 1));
+    N = size(A,3);
+    e_vec = zeros(4,N);
 
-    if phi == 0
-        % assume spin about z-axis
-        e_vec = [0;0;1];
-    else
-        e1 = (A(2,3) - A(3,2))/(2*sin(phi));
-        e2 = (A(3,1) - A(1,3))/(2*sin(phi));
-        e3 = (A(1,2) - A(2,1))/(2*sin(phi));
-        e_vec = [e1;e2;e3];
+    for n = 1:N
+        phi = acos(0.5 * (trace(A(:,:,n)) - 1));
+        if phi == 0
+            % assume spin about z-axis
+            e1 = 0;
+            e2 = 0;
+            e3 = 1;
+        else
+            e1 = (A(2,3) - A(3,2,n))/(2*sin(phi));
+            e2 = (A(3,1) - A(1,3,n))/(2*sin(phi));
+            e3 = (A(1,2) - A(2,1,n))/(2*sin(phi));
+        end
+        e_vec(:,n) = [e1;e2;e3;phi];
     end
 
 end
--- propagation/dcm2quat.m
+++ propagation/dcm2quat.m
@@ -1,20 +1,23 @@
 function q = dcm2quat(A)
     % convert the dcm to quaterion 4x1 representation
 
-    phi = acos(0.5 * (trace(A) - 1));
+    N = size(A,3);
+    q = zeros(4,N);
 
-    if phi == 0
-        q = [0;0;0;1];
-    else
-        e1 = (A(2,3) - A(3,2))/(2*sin(phi));
-        e2 = (A(3,1) - A(1,3))/(2*sin(phi));
-        e3 = (A(1,2) - A(2,1))/(2*sin(phi));
-        q = [e1*sin(phi/2);
-             e2*sin(phi/2);
-             e3*sin(phi/2);
-             cos(phi/2)];
+    for n = 1:N
+        phi = acos(0.5 * (trace(A(:,:,n)) - 1));
+        if phi == 0
+            qns = [0;0;0;1];
+        else
+            e1 = (A(2,3) - A(3,2,n))/(2*sin(phi));
+            e2 = (A(3,1) - A(1,3,n))/(2*sin(phi));
+            e3 = (A(1,2) - A(2,1,n))/(2*sin(phi));
+            qns = [e1*sin(phi/2);
+                  e2*sin(phi/2);
+                  e3*sin(phi/2);
+                  cos(phi/2)];
+        end
+        q(:,n) = qns./norm(qns);
     end
-    
-    q = q./norm(q);
 
 end
