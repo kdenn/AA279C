4a2f6670c3273c0c388af56aadf6d0b11fa68553
Kaitlin Dennison <kdenn@stanford.edu>
2021-04-19 11:48:42 -0700

    update quat2dcm to handle array of qns

--- propagation/quat2dcm.m
+++ propagation/quat2dcm.m
@@ -3,15 +3,21 @@ function DCM = quat2dcm(quat)
 % coordinates to principal axes
 
 % q1 = quat(1); q2 = quat(2); q3 = quat(3); q4 = quat(4);
-% qv = quat(1:3); qs = quat(4); 
+% qv = quat(1:3); qs = quat(4);
 % q_cross = [  0, -q3, +q2; ...
 %            +q3,   0, -q1; ...
 %            -q2, +q1,   0];
 % DCM = diag(q4^2 - qv.^2) + 2.*(qv*qv') - 2.*q4.*q_cross;
 
 % Method used in Diebel Attitude document
-q1 = quat(1); q2 = quat(2); q3 = quat(3); q0 = quat(4);
-DCM =  [(q0^2+q1^2-q2^2-q3^2), (2*q1*q2+2*q0*q3), (2*q1*q3-2*q0*q2); ...
-        (2*q1*q2-2*q0*q3), (q0^2-q1^2+q2^2-q3^2), (2*q2*q3+2*q0*q1); ...
-        (2*q1*q3+2*q0*q2), (2*q2*q3-2*q0*q1), (q0^2-q1^2-q2^2+q3^2)];
+
+	N = size(quat,1);
+	DCM = zeros(3,3,N);
+
+	for n = 1:N
+		q1 = quat(1,n); q2 = quat(2,n); q3 = quat(3,n); q0 = quat(4,n);
+		DCM(:,:,n) =  [(q0^2+q1^2-q2^2-q3^2), (2*q1*q2+2*q0*q3), (2*q1*q3-2*q0*q2); ...
+				(2*q1*q2-2*q0*q3), (q0^2-q1^2+q2^2-q3^2), (2*q2*q3+2*q0*q1); ...
+				(2*q1*q3+2*q0*q2), (2*q2*q3-2*q0*q1), (q0^2-q1^2-q2^2+q3^2)];
+	end
 end
