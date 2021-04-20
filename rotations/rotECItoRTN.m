633af474d565e30a28cb6b6b18ba808190a1e3d9
Kaitlin Dennison <kdenn@stanford.edu>
2021-04-20 08:48:04 -0700

    calculate nu from r

--- rotations/rotECItoRTN.m
+++ rotations/rotECItoRTN.m
@@ -1,3 +1,15 @@
-function R = rotECItoRTN(Om,incl,w,e,nu)
-R = R3(nu)*rotPQWtoIJK(Om,incl,w,e)';
+function R = rotECItoRTN(Om,incl,w,e,nu_or_r)
+
+R_P2E = rotPQWtoIJK(Om,incl,w,e);
+
+if numel(nu_or_r) > 1
+    r_E = nu_or_r;
+    r_P = R_P2E'*r_E;
+    nu = atan2(r_P(2),r_P(1));
+else
+    nu = nu_or_r;
+end 
+ 
+R = R3(nu)*R_P2E';
+
 end
