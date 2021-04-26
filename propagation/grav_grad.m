ad2cc48041a72597c8158db4c46b43a0e9f4b9b4
Kaitlin Dennison <kdenn@stanford.edu>
2021-04-26 15:22:13 -0700

    indents

--- propagation/grav_grad.m
+++ propagation/grav_grad.m
@@ -10,8 +10,8 @@ Ix = I_princ(1,1); Iy = I_princ(2,2); Iz = I_princ(3,3);
 R = norm(r);
 c = r./R;
 cx = c(1); cy = c(2); cz = c(3);
-M = (3*mu)/(R^3) * [(Iz - Iy)*cy*cz;
-                    (Ix - Iz)*cz*cx;
-                    (Iy - Ix)*cx*cy];
+M = ((3*mu)/(R^3)) * [(Iz - Iy)*cy*cz;
+                      (Ix - Iz)*cz*cx;
+                      (Iy - Ix)*cx*cy];
                 
 end
