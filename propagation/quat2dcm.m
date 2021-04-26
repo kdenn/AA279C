dfca9cddcb69a0f987f1b775246c24f3b530c3a8
Kaitlin Dennison <kdenn@stanford.edu>
2021-04-26 15:28:52 -0700

    change to vector math

--- propagation/quat2dcm.m
+++ propagation/quat2dcm.m
@@ -1,6 +1,6 @@
-function DCM = quat2dcm(quat)
-% Given input quaternion attitude, outputs the DCM from inertial
-% coordinates to principal axes
+function DCM = quat2dcm(q)
+% Given input quaternion attitude, outputs the DCM from reference frame to
+% the principal axis
 
 % q1 = quat(1); q2 = quat(2); q3 = quat(3); q4 = quat(4);
 % qv = quat(1:3); qs = quat(4);
@@ -11,14 +11,16 @@ function DCM = quat2dcm(quat)
 
 % Method used in Diebel Attitude document
 
-	N = size(quat,2);
+	N = size(q,2);
 	DCM = zeros(3,3,N);
 
 	for n = 1:N
-		q1 = quat(1,n); q2 = quat(2,n); q3 = quat(3,n); q0 = quat(4,n);
-		DCM_pre =  [(q0^2+q1^2-q2^2-q3^2), (2*q1*q2+2*q0*q3), (2*q1*q3-2*q0*q2); ...
-				(2*q1*q2-2*q0*q3), (q0^2-q1^2+q2^2-q3^2), (2*q2*q3+2*q0*q1); ...
-				(2*q1*q3+2*q0*q2), (2*q2*q3-2*q0*q1), (q0^2-q1^2-q2^2+q3^2)];
-        DCM(:,:,n) = DCM_pre./vecnorm(DCM_pre,2,1);
+        qvec = [q(1); q(2); q(3)];
+        qcross = [ 0 -q(3) q(2);...
+                   q(3) 0 -q(1);...
+                   -q(2) q(1) 0];
+        A =(q(4)^2-norm(qvec)^2)*eye(3)+2*(qvec*qvec.')-2*q(4)*qcross;
+        column1 = A(:,1); column2 = A(:,2); column3 = A(:,3); 
+        DCM(:,:,n)=[column1/norm(column1) column2/norm(column2) column3/norm(column3)];
 	end
 end
