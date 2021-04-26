1cd8bbe239535ba345c902ba37abca0627457b32
Kaitlin Dennison <kdenn@stanford.edu>
2021-04-26 10:20:20 -0700

    Incorporate torque into Euler

--- propagation/int_Euler_eqs.m
+++ propagation/int_Euler_eqs.m
@@ -1,11 +1,15 @@
-function ydot = int_Euler_eqs(t,y,I_princ)
+function ydot = int_Euler_eqs(t,y,I_princ,M)
 % Integrate Euler's Equations of Motion for angular momentum in principal
 % axes
 
 % Renaming variables
 Ix = I_princ(1,1); Iy = I_princ(2,2); Iz = I_princ(3,3);
 wx = y(1); wy = y(2); wz = y(3);
-Mx = 0; My = 0; Mz = 0;
+if nargin == 3
+    Mx = 0; My = 0; Mz = 0;
+else
+    Mx = M(1); My = M(2); Mz = M(3);
+end
 
 % State space form
 ydot = zeros(3,1);
