f3779286cb6997505c197a392815377dc792cd81
Kaitlin Dennison <kdenn@stanford.edu>
2021-05-03 01:24:01 -0700

    initial work on magnetic torque

---
+++ ps_5.m
@@ -0,0 +1,54 @@
+% Kaitlin Dennison
+% Joshua Geiser
+
+clear all; close all; clc;
+
+%% 5-1)
+%{
+    In addition to gravity gradient, program perturbation torques due to
+    magnetic field, solar radiation pressure, and  atmospheric  drag. Note
+    1: You  can  apply  a  very  minimal/basic  model  for  perturbations
+    that are  not  relevant (negligible) to your project.Note 2: All
+    perturbations can be grouped into a single large subsystem called
+    environment or similar whose output feed the Euler equations. Note 3:
+    Re-use as many functions as possible for solar radiation pressure and
+    atmospheric drag.
+%}
+
+% TODO
+
+%% 5-2)
+%{
+    Include all torques in numerical integration. Please show comparison of
+    numerically computed disturbance torques with expected values and trend
+    from theory (model) and tables (Wertz). Plot all torque components in
+    principal axes over time. Plot the resultant (sum) of all torques in
+    principal axes. Make sure that your model is not too ideal, i.e. make
+    sure that center of pressure and center of mass do not coincide.
+%}
+
+% TODO
+
+%% 5-3)
+%{
+    Now you can compute the attitude control error even if a controller is
+    not implemented yet. The attitude control error represents the rotation
+    between the desired and actual attitude. Plot the attitude control
+    error and  give its  interpretation.  Note  that  this  step  requires
+    the  definition and  computation  of  the  desired  or  nominal or
+    target attitude of the spacecraft. In general, this can be expressed in
+    body or principal axes.
+%}
+
+% TODO
+
+%% 3-4)
+%{
+    Note that the attitude control error represents a rotation matrix (DCM)
+    which quantifies how far the actualattitude  is  from  the  true
+    attitude. You  can  use  any  parameterization to  plot  the  attitude
+    control errors corresponding to this DCM. Give interpretation of the
+    attitude control errors given the applied disturbances.
+%}
+
+% TODO
