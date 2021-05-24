7acb3ffda67bd288fc2cbb9842981c04bc9f6d35
Kaitlin Dennison <kdenn@stanford.edu>
2021-05-24 12:12:07 -0700

    cleanup

--- @Visors/UKF/fstate.m
+++
@@ -1,3 +0,0 @@
-function xACAF = fstate(xACAF)
-% xf = RotVert(xACAF,ast.alp,ast.del,ast.W_0,ast.W_d,JD,false);
-end
--- @Visors/UKF/hmeas.m
+++
@@ -1,8 +0,0 @@
-function z = hmeas(xACAF,rSat,R_ACAF2cam,cam)
-% Convert 3D ACAF coordinate to 2D im
-% xACI = RotVert(xACAF',ast.alp,ast.del,ast.W_0,ast.W_d,JD,false);
-% z = (transPt32(xACI,rSat,R,cam))';
-% xACI = ast.RotACAF2ACI(JD) * xACAF;
-t = R_ACAF2cam * -rSat;
-z = cam.ProjectiveTransform(xACAF,R_ACAF2cam,t);
-end
