9c19782edc4f16b43c99aa14eae5d7539b5d9df0
Kaitlin Dennison <kdenn@stanford.edu>
2021-05-25 18:01:13 -0700

    update friction (again)

--- @Visors/actuate_RW.m
+++ @Visors/actuate_RW.m
@@ -1,4 +1,4 @@
-function M_act = actuate_RW(obj, M_des, add_noise)
+function M_act = actuate_RW(obj, M_des, w_sc, dt, add_noise)
 % Purpose: Reaction wheel actuator modeling with added Gaussian noise
 
 if ~exist('add_noise','var')
@@ -28,17 +28,17 @@ end
 % Wertz 7.9
 N = numel(L_dot_cmd);
 
-I = 0.0001147 * ones(4,1);
-w = L_dot_cmd./I;
-s = w./(2*pi); % rpm
-
+% Max torque
 sn = sign(L_dot_cmd);
-
 L_dot_act = L_dot_cmd;
 for i = 1:N
     L_dot_act(i) = min(L_dot_max,abs(L_dot_act(i)));
 end
-
+% Each RW has an inertia of 0.0001147 kg-m^2 (calculated from m = 0.130 kg
+% and V = 42x42x19 mm)
+I = 0.0001147 * ones(4,1);
+w_d = (sn.*L_dot_act./I)*dt + A_star * w_sc;
+s = abs(60.*w_d./(2*pi)); % rpm
 M_c = 0; % Coulomb friction coefficient (Nm)
 f = 1.21E-6; % viscous friction coefficient (Nm/rpm)
 M_f = M_c + f.*abs(s);
@@ -47,15 +47,11 @@ for i = 1:N
     L_dot_out(i,1) = sn(i)*max(0,M_out(i));
 end
 
-% 1-sigma noise of 0.01 rad/s for angular velocity
-% Each RW has an inertia of 0.0001147 kg-m^2 (calculated from m = 0.130 kg
-% and V = 42x42x19 mm)
-I = 0.0001147 * ones(4,1);
-w0 = zeros(4,1);
-Q = 0.0005^2 * eye(1);
+% 1-sigma noise for angular velocity
+Q = (10^(-5))^2 * eye(1);
 w_dot_noise = sqrtm(Q)*randn(4,1);
 
 % Actual moment imparted on s/c
-L_dot_noise = I .* (w0 + w_dot_noise) .* (L_dot_out~=0);
+L_dot_noise = I .* w_dot_noise .* (L_dot_out~=0);
 M_act = A * (L_dot_out + L_dot_noise);
 end
